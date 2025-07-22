bad2340ce7badf0894ea2ab361acc9327903696b
roro3828 <github@roro.ro>
2025-07-22 12:26:42 +0900

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,16 @@ export default function Home() {
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUUID(event.target.value)
     }
+    const handleRegenerate = (event: React.MouseEventHandler<HTMLInputElement>) => {
+        setUUID(new UUID().toString());
+    }
 
     return (
         <div className=" w-full" style={{
             "imageRendering": "pixelated"
         }}>
             <div className="w-1/3 m-auto">
+                <input onClick={handleRegenerate} className="m-auto w-1/6 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button" value={"Regenerate"}></input>
                 <input className="w-full" value={getUUID} onChange={handleInputChange} type="text"></input>
                 <Image style={{"imageRendering": "pixelated"}} className="w-full p-3" width={64} height={64} src={`${process.env.NEXT_PUBLIC_ICON_GENERATE_URL}`+getUUID} alt="auto generated icon"/>
 
